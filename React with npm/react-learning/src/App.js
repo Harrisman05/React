979e66b3b903fcc7e398690289070ae1a727cdec
harle <harleyharris05@hotmail.com>
2022-05-15 10:00:17 +0100

    Added logo and more list items

--- React with npm/react-learning/src/App.js
+++ React with npm/react-learning/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import logo from './favicon.ico';
 
 function App() {
   return (
+
     <div className="App">
-    <h1> Hello world </h1>
+      <img src={logo} alt="React icon"/>
+      <h1> Fun facts about React </h1>
+      <ol>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ol>
+      <h1>Reasons I'm learning React</h1>
+      <ol>
+        <li>Most popular JS framework</li>
+        <li>Want to level up web dev skills</li>
+        <li>Practise using npm, import, export</li>
+      </ol>
     </div>
   );
 }
 
 export default App;
+
